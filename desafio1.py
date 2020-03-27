a570190507ae038fe7fc7a856386e9f609933476
thisisFlawsome <m.violapiccollo@gmail.com>
2020-03-27 11:32:58 -0300

    Desafio1 Completado

--- desafio1.py
+++ desafio1.py
@@ -1,8 +1,22 @@
 #Escribir un algoritmo que encuentre el máximo y el minímo número dentro de una lista de números enteros desordenados
 
-lista = (65,4,2,88,80,3,6,100,1)
+lista = [65,4,3,88,80,2,6,100,7]
 
-mas_bajo = min(lista)
-print(mas_bajo)
-mas_alto = max(lista)
-print(mas_alto)
+# mas_bajo = min(lista)
+# print(mas_bajo)
+# mas_alto = max(lista)
+# print(mas_alto)
+
+
+menor = lista[0]
+mayor = lista[0]
+for i in lista: 
+    if i < menor:
+        menor = i
+    if i > mayor:  
+        mayor = i
+print(f"{menor}, {mayor}")
+
+
+
+
